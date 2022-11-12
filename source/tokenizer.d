4ef1a986339bc5f2d7496083e5d4c3b2a8281c40
dk949 <56653556+dk949@users.noreply.github.com>
2022-11-12 15:17:11 +0000

    using indices to build tokens

--- source/tokenizer.d
+++ source/tokenizer.d
@@ -2,9 +2,11 @@ module tokenizer;
 
 import std.exception;
 import std.algorithm;
-import std.uni;
 import std.traits;
 import std.conv;
+import std.range;
+import std.exception;
+import std.uni;
 
 enum TokenType {
     ID,
@@ -26,11 +28,12 @@ class TokenException : Exception {
 struct Token {
     TokenType type;
     Payload payload;
-    bool opEquals(TokenType t) const {
+
+    bool opEquals(TokenType t) const pure nothrow {
         return t == type;
     }
 
-    bool opEquals(N)(N num) const
+    bool opEquals(N)(N num) const pure nothrow
     if (isNumeric!N) {
         if (type == TokenType.NUMBER)
             return payload.num == num;
@@ -38,7 +41,7 @@ struct Token {
             return false;
     }
 
-    bool opEquals(S)(S str) const
+    bool opEquals(S)(S str) const pure nothrow
     if (isSomeString!S) {
         if (type == TokenType.ID)
             return payload.str == str.text;
@@ -46,7 +49,7 @@ struct Token {
             return false;
     }
 
-    bool opEquals()(auto ref const Token other) const {
+    bool opEquals()(auto ref const Token other) const pure nothrow {
         if (type == other.type)
             with (TokenType) final switch (type) {
             case ID:
@@ -60,18 +63,18 @@ struct Token {
             return false;
     }
 
-    this(S)(S str)
+    this(S)(S str) pure nothrow
     if (isNarrowString!S) {
         type = TokenType.ID;
         payload.str = str.text;
     }
 
-    this(double num) {
+    this(double num) pure nothrow {
         type = TokenType.NUMBER;
         payload.num = num;
     }
 
-    this(TokenType tok) {
+    this(TokenType tok) pure nothrow {
         type = tok;
         with (TokenType) final switch (tok) {
             case ID:
@@ -87,12 +90,12 @@ struct Token {
         }
     }
 
-    string toString() const {
+    string toString() const pure nothrow {
         with (TokenType) final switch (type) {
             case ID:
                 return "Id(" ~ payload.str ~ ")";
             case NUMBER:
-                return "Number(" ~ payload.num.text ~ ")";
+                return "Number(" ~ payload.num.text.assertNotThrown ~ ")";
             case LBRACKET:
                 return "LBRACKET";
             case RBRACKET:
@@ -100,7 +103,7 @@ struct Token {
         }
     }
 
-    auto to(TokenType tok)() {
+    auto to(TokenType tok)() const pure {
         if (type == tok) {
             static if (tok == TokenType.NUMBER)
                 return payload.num;
@@ -111,41 +114,75 @@ struct Token {
     }
 }
 
-bool isSpaceStr(S)(S str) {
-    return all!(isSpace)(str);
+bool isSpaceStr(S)(S str) pure nothrow {
+    return str.all!isWhite.assertNotThrown;
+}
+
+unittest {
+    auto trueTests = [
+        "empty": "",
+        "one space": " ",
+        "two spaces": "  ",
+        "newline": "\n",
+        "space and newline": " \n",
+    ];
+
+    auto falseTests = [
+        "one char": "a",
+        "two chars": "ab",
+        "char and space": " b",
+        "char and space 2": "b ",
+        "many spaces and char": "    \n    \t     )        ",
+    ];
+
+    foreach (msg, test; trueTests)
+        assert(test.isSpaceStr, msg);
+
+    foreach (msg, test; falseTests)
+        assert(!test.isSpaceStr, msg);
 
 }
 
-Token[] tokenize(string chars) {
-    char[] curr;
+Token[] tokenize(string chars) pure nothrow {
+    long currStart, currEnd;
     Token[] outp;
 
-    auto pushCurr() {
-        if (!isSpaceStr(curr))
-            try
-                outp ~= Token(curr.to!double);
-            catch (ConvException)
-                outp ~= Token(curr);
+    auto pushCurr() nothrow {
+        const tmp = chars[currStart .. currEnd];
+        if (!isSpaceStr(tmp))
+            outp ~= Token(tmp.to!double)
+                .ifThrown!ConvException(Token(tmp))
+                .assertNotThrown;
+        currStart = currEnd;
+    }
 
-        curr.length = 0;
+    auto skipCurr() nothrow {
+        assert(currStart == currEnd);
+        currStart++;
+        currEnd++;
     }
 
     foreach (ch; chars) {
+        import std.stdio;
+
         switch (ch) {
             case ' ':
             case '\0': .. case '\v':
                 pushCurr;
+                skipCurr;
                 break;
             case '(':
                 pushCurr;
+                skipCurr;
                 outp ~= Token(TokenType.LBRACKET);
                 break;
             case ')':
                 pushCurr;
+                skipCurr;
                 outp ~= Token(TokenType.RBRACKET);
                 break;
             default:
-                curr ~= (ch);
+                currEnd++;
                 break;
         }
     }
@@ -161,6 +198,9 @@ unittest {
         const empty = "".tokenize();
         const wsempty = "  \n".tokenize();
 
+        assert(empty.length == 0, "Empty string should produce no tokens");
+        assert(wsempty.length == 0, "String with just spaces should produce no tokens");
+
         // 1 token
         const lbracket = "(".tokenize();
         const rbracket = ")".tokenize();
@@ -175,41 +215,50 @@ unittest {
         const pscin = "1.7e-10".tokenize();
         const nscin = "-1.7e-10".tokenize();
 
+        assert(lbracket.length == 1 && lbracket[0] == LBRACKET, "left brakcet");
+        assert(rbracket.length == 1 && rbracket[0] == RBRACKET, "right brakcet");
+        assert(id.length == 1 && id[0] == ID && id[0] == "hello", "ID");
+        assert(charsInId.length == 1 && charsInId[0] == ID && charsInId[0] == "89yasdf98u3_/..]][]]\\@@$%^&*", "Id with weird cgaracters");
+        assert(pinteger.length == 1 && pinteger[0] == NUMBER && pinteger[0] == 37, "positive integer");
+        assert(ninteger.length == 1 && ninteger[0] == NUMBER && ninteger[0] == -37, "negative integer");
+        assert(pfloating.length == 1 && pfloating[0] == NUMBER && pfloating[0] == 37.89, "positive float");
+        assert(nfloating.length == 1 && nfloating[0] == NUMBER && nfloating[0] == -37.89, "negative float");
+        assert(pscip.length == 1 && pscip[0] == NUMBER && pscip[0] == 1.7e10, "positive mantissa positive exponent");
+        assert(nscip.length == 1 && nscip[0] == NUMBER && nscip[0] == -1.7e10, "negative mantissa positive exponent");
+        assert(pscin.length == 1 && pscin[0] == NUMBER && pscin[0] == 1.7e-10, "positive mantissa negative exponent");
+        assert(nscin.length == 1 && nscin[0] == NUMBER && nscin[0] == -1.7e-10, "negative mantissa negative exponent");
+
         // Many tokens
         const openOpenCLose = "(()".tokenize();
         const wsopenOpenCLose = "   (  (  \n\t)\n ".tokenize();
         const closeIdNumOpen = ")i 27(".tokenize();
         const wscloseIdNumOpen = "  )  i\n27\t(".tokenize();
+        const define = "(define hello 47)".tokenize();
 
-        assert(empty.length == 0);
-        assert(wsempty.length == 0);
-
-        // 1 token
-        assert(lbracket.length == 1 && lbracket[0] == LBRACKET);
-        assert(rbracket.length == 1 && rbracket[0] == RBRACKET);
-        assert(id.length == 1 && id[0] == ID && id[0] == "hello");
-        assert(charsInId.length == 1 && charsInId[0] == ID && charsInId[0] == "89yasdf98u3_/..]][]]\\@@$%^&*");
-        assert(pinteger.length == 1 && pinteger[0] == NUMBER && pinteger[0] == 37);
-        assert(ninteger.length == 1 && ninteger[0] == NUMBER && ninteger[0] == -37);
-        assert(pfloating.length == 1 && pfloating[0] == NUMBER && pfloating[0] == 37.89);
-        assert(nfloating.length == 1 && nfloating[0] == NUMBER && nfloating[0] == -37.89);
-        assert(pscip.length == 1 && pscip[0] == NUMBER && pscip[0] == 1.7e10);
-        assert(nscip.length == 1 && nscip[0] == NUMBER && nscip[0] == -1.7e10);
-        assert(pscin.length == 1 && pscin[0] == NUMBER && pscin[0] == 1.7e-10);
-        assert(nscin.length == 1 && nscin[0] == NUMBER && nscin[0] == -1.7e-10);
-
-        // Many tokens
         assert(openOpenCLose == [
-                Token(LBRACKET), Token(LBRACKET), Token(RBRACKET)
-            ]);
+                Token(LBRACKET), Token(LBRACKET), Token(RBRACKET),
+            ],
+            "left, left, right"
+        );
         assert(wsopenOpenCLose == [
-                Token(LBRACKET), Token(LBRACKET), Token(RBRACKET)
-            ]);
+                Token(LBRACKET), Token(LBRACKET), Token(RBRACKET),
+            ], "left, left, right despite white space");
+
         assert(closeIdNumOpen == [
-                Token(RBRACKET), Token("i"), Token(27), Token(LBRACKET)
-            ]);
+                Token(RBRACKET), Token("i"), Token(27), Token(LBRACKET),
+            ],
+            "right, id = i, num = 27, left"
+        );
         assert(wscloseIdNumOpen == [
-                Token(RBRACKET), Token("i"), Token(27), Token(LBRACKET)
-            ]);
+                Token(RBRACKET), Token("i"), Token(27), Token(LBRACKET),
+            ],
+            "right, id = i, num = 27, left, despite white space"
+        );
+        assert(define == [
+                Token(LBRACKET), Token("define"), Token("hello"), Token(47),
+                Token(RBRACKET)
+            ],
+            "left, id = define, id = hello, num = 47, right"
+        );
     }
 }
