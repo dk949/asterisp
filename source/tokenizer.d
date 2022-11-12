461bf3cefe6619417fead017d4f56750ce33dd79
dk949 <56653556+dk949@users.noreply.github.com>
2022-11-12 23:49:45 +0000

    rewrote tokeniser as an actual tokenizer

--- source/tokenizer.d
+++ source/tokenizer.d
@@ -1,5 +1,8 @@
 module tokenizer;
 
+import utils;
+import errors;
+
 import std.exception;
 import std.algorithm;
 import std.traits;
@@ -7,10 +10,12 @@ import std.conv;
 import std.range;
 import std.exception;
 import std.uni;
+import std.stdio;
 
 enum TokenType {
     ID,
     NUMBER,
+    STRING,
     LBRACKET,
     RBRACKET,
 }
@@ -49,13 +54,15 @@ struct Token {
             return false;
     }
 
-    bool opEquals()(auto ref const Token other) const pure nothrow {
+    bool opEquals(Token other) const pure nothrow {
         if (type == other.type)
             with (TokenType) final switch (type) {
             case ID:
                 return payload.str == other.payload.str;
             case NUMBER:
                 return payload.num == other.payload.num;
+            case STRING:
+                return payload.str == other.payload.str;
             case LBRACKET:
             case RBRACKET:
                 return true;
@@ -83,6 +90,9 @@ struct Token {
             case NUMBER:
                 payload.num = payload.num.init;
                 break;
+            case STRING:
+                payload.str = payload.str.init;
+                break;
             case LBRACKET:
             case RBRACKET:
                 payload.null_ = payload.null_.init;
@@ -96,6 +106,8 @@ struct Token {
                 return "Id(" ~ payload.str ~ ")";
             case NUMBER:
                 return "Number(" ~ payload.num.text.assertNotThrown ~ ")";
+            case STRING:
+                return "String(" ~ payload.str ~ ")";
             case LBRACKET:
                 return "LBRACKET";
             case RBRACKET:
@@ -143,50 +155,209 @@ unittest {
 
 }
 
-Token[] tokenize(string chars) pure nothrow {
-    long currStart, currEnd;
+private alias CharIt = immutable(char)*;
+
+private enum State {
+    Begin,
+    Comment,
+    ID,
+    Number,
+    Minus,
+    StringStart,
+    String,
+}
+
+Token[] tokenize(string chars) {
     Token[] outp;
+    char[] curr;
 
-    auto pushCurr() nothrow {
-        const tmp = chars[currStart .. currEnd];
-        if (!isSpaceStr(tmp))
-            outp ~= Token(tmp.to!double)
-                .ifThrown!ConvException(Token(tmp))
-                .assertNotThrown;
-        currStart = currEnd;
+    if (chars.empty)
+        return outp;
+
+    auto state = State.Begin;
+    auto ch = chars.begin;
+
+    auto toState(State s) {
+        state = s;
     }
 
-    auto skipCurr() nothrow {
-        assert(currStart == currEnd);
-        currStart++;
-        currEnd++;
+    auto consume() {
+        curr ~= *ch;
+        ch++;
     }
 
-    foreach (ch; chars) {
-        import std.stdio;
+    auto drop() {
+        ch++;
+    }
 
-        switch (ch) {
-            case ' ':
-            case '\0': .. case '\v':
-                pushCurr;
-                skipCurr;
+    auto dupe() {
+        curr ~= *ch;
+    }
+
+    auto store(State st) {
+        final switch (st) {
+            case State.Number:
+                try
+                    outp ~= Token(curr.to!double);
+                catch (ConvException)
+                    throw new TokenException("Invalid number " ~ curr.idup);
                 break;
+            case State.String:
+                throw new TokenException("Strings not yet supported");
+            case State.ID:
+                outp ~= Token(curr);
+                break;
+            case State.Begin:
+            case State.Minus:
+            case State.Comment:
+            case State.StringStart:
+                throw new InternalError("Cannot store state " ~ st.text);
+        }
+        curr.clear;
+    }
+
+    auto sBegin() {
+        switch (*ch) {
             case '(':
-                pushCurr;
-                skipCurr;
                 outp ~= Token(TokenType.LBRACKET);
+                drop;
                 break;
             case ')':
-                pushCurr;
-                skipCurr;
                 outp ~= Token(TokenType.RBRACKET);
+                drop;
+                break;
+            case ' ':
+            case '\0': .. case '\v':
+                drop;
+                break;
+            case ';':
+                toState(State.Comment);
+                break;
+            case '0': .. case '9':
+                toState(State.Number);
+                break;
+            case '-':
+                toState(State.Minus);
+                break;
+            case '"':
+                toState(State.StringStart);
                 break;
             default:
-                currEnd++;
+                toState(State.ID);
                 break;
         }
     }
-    pushCurr;
+
+    auto sComment() {
+        if (*ch == '\n')
+            toState(State.Begin);
+        drop;
+    }
+
+    auto sID() {
+        switch (*ch) {
+            case '"':
+                throw new TokenException("unsexpected \" when parsing ID");
+            case '(':
+            case ')':
+            case ' ':
+            case '\0': .. case '\v':
+                store(State.ID);
+                toState(State.Begin);
+                break;
+            default:
+                consume;
+        }
+    }
+
+    auto sNumber() {
+        switch (*ch) {
+            case '0': .. case '9':
+            case '.':
+            case '-':
+            case 'e': // Scientific notation
+                consume;
+                break;
+            default:
+                toState(State.Begin);
+                store(State.Number);
+                break;
+        }
+    }
+
+    auto sMinus() {
+        if (*ch != '-')
+            throw new InternalError("Expected - found " ~ *ch);
+        if (ch + 1 != chars.end && isNumber(*(ch + 1)))
+            toState(State.Number);
+        else
+            toState(State.ID);
+
+    }
+
+    auto sStringStart() {
+        if (*ch != '"')
+            throw new InternalError("Expected \" found " ~ *ch);
+        drop;
+        toState(State.String);
+    }
+
+    auto sString() {
+        switch (*ch) {
+            case '"':
+                drop;
+                toState(State.Begin);
+                store(State.String);
+                break;
+            case '\\':
+                throw new TokenException("Escape characters not yet supported");
+            default:
+                consume;
+        }
+        drop;
+        toState(State.String);
+    }
+
+    while (ch != chars.end) {
+        final switch (state) {
+            case State.Begin:
+                sBegin;
+                break;
+            case State.Comment:
+                sComment;
+                break;
+            case State.ID:
+                sID;
+                break;
+            case State.Number:
+                sNumber;
+                break;
+            case State.Minus:
+                sMinus;
+                break;
+            case State.StringStart:
+                sStringStart;
+                break;
+            case State.String:
+                sString;
+                break;
+        }
+    }
+    final switch (state) {
+        case State.StringStart:
+            throw new TokenException("Unexpected \" at end of input");
+        case State.Minus:
+            throw new InternalError("Unstored Minus at end of input");
+        case State.String:
+            throw new TokenException("Unterminated string at end of input");
+        case State.ID:
+        case State.Number:
+            store(state);
+            break;
+        case State.Comment:
+        case State.Begin: // Do nothing
+            break;
+    }
 
     return outp;
 }
@@ -205,11 +376,12 @@ unittest {
         const lbracket = "(".tokenize();
         const rbracket = ")".tokenize();
         const id = "hello".tokenize();
-        const charsInId = "89yasdf98u3_/..]][]]\\@@$%^&*".tokenize();
+        const charsInId = "yasdf98u3_/..]][]]\\@@$%^&*".tokenize();
         const pinteger = "37".tokenize();
         const ninteger = "-37".tokenize();
         const pfloating = "37.89".tokenize();
         const nfloating = "-37.89".tokenize();
+        const traildec = "37.".tokenize();
         const pscip = "1.7e10".tokenize();
         const nscip = "-1.7e10".tokenize();
         const pscin = "1.7e-10".tokenize();
@@ -218,15 +390,22 @@ unittest {
         assert(lbracket.length == 1 && lbracket[0] == LBRACKET, "left brakcet");
         assert(rbracket.length == 1 && rbracket[0] == RBRACKET, "right brakcet");
         assert(id.length == 1 && id[0] == ID && id[0] == "hello", "ID");
-        assert(charsInId.length == 1 && charsInId[0] == ID && charsInId[0] == "89yasdf98u3_/..]][]]\\@@$%^&*", "Id with weird cgaracters");
+        assert(charsInId.length == 1 && charsInId[0] == ID && charsInId[0] == "yasdf98u3_/..]][]]\\@@$%^&*", "Id with weird characters");
         assert(pinteger.length == 1 && pinteger[0] == NUMBER && pinteger[0] == 37, "positive integer");
         assert(ninteger.length == 1 && ninteger[0] == NUMBER && ninteger[0] == -37, "negative integer");
         assert(pfloating.length == 1 && pfloating[0] == NUMBER && pfloating[0] == 37.89, "positive float");
         assert(nfloating.length == 1 && nfloating[0] == NUMBER && nfloating[0] == -37.89, "negative float");
+        assert(traildec.length == 1 && traildec[0] == NUMBER && traildec[0] == 37., "trailing decimal point");
         assert(pscip.length == 1 && pscip[0] == NUMBER && pscip[0] == 1.7e10, "positive mantissa positive exponent");
         assert(nscip.length == 1 && nscip[0] == NUMBER && nscip[0] == -1.7e10, "negative mantissa positive exponent");
         assert(pscin.length == 1 && pscin[0] == NUMBER && pscin[0] == 1.7e-10, "positive mantissa negative exponent");
         assert(nscin.length == 1 && nscin[0] == NUMBER && nscin[0] == -1.7e-10, "negative mantissa negative exponent");
+
+        assertThrown!TokenException("1.2.3".tokenize, "too many decimal points");
+        assertThrown!TokenException("1e5e5".tokenize, "too many e");
+        assertThrown!TokenException("1e".tokenize, "trailing e");
+        assertThrown!TokenException("1e.".tokenize, "trailing e and decimal point");
+        assertThrown!TokenException("1.e".tokenize, "trailing decimal point and e");
 
         // Many tokens
         const openOpenCLose = "(()".tokenize();
@@ -234,6 +413,7 @@ unittest {
         const closeIdNumOpen = ")i 27(".tokenize();
         const wscloseIdNumOpen = "  )  i\n27\t(".tokenize();
         const define = "(define hello 47)".tokenize();
+        const subtract = "(- 49 hello)".tokenize();
 
         assert(openOpenCLose == [
                 Token(LBRACKET), Token(LBRACKET), Token(RBRACKET),
@@ -259,6 +439,12 @@ unittest {
                 Token(RBRACKET)
             ],
             "left, id = define, id = hello, num = 47, right"
+        );
+        assert(subtract == [
+                Token(LBRACKET), Token("-"), Token(49), Token("hello"),
+                Token(RBRACKET)
+            ],
+            "left, id = -, num = 49, id = hello, right"
         );
     }
 }
--- source/utils.d
+++ source/utils.d
@@ -4,6 +4,8 @@ import errors;
 import types;
 
 import std.conv;
+import std.traits;
+import std.range;
 
 T forceCast(T, W)(W what) {
     if (auto output = cast(T) what) {
@@ -57,4 +59,28 @@ T construct(T, Args...)(Args args) {
 auto ref dbg(string msg, T)(auto ref T t) {
     stderr.writeln(msg, t);
     return t;
+}
+
+ElementType!R* begin(R)(auto ref R r)
+if (!isSomeString!R && isInputRange!R) {
+    return &r.front;
+}
+
+ElementType!R* end(R)(auto ref R r)
+if (!isSomeString!R && isBidirectionalRange!R) {
+    return (&r.back) + 1;
+}
+
+auto begin(R)(auto ref R r)
+if (isSomeString!R) {
+    return &r[0];
+}
+
+auto end(R)(auto ref R r)
+if (isSomeString!R) {
+    return (&r[$ - 1]) + 1;
+}
+
+void clear(R)(ref R r){
+    r.length = 0;
 }
