ad4f3d2a0c2cc2f28e1b968925a53a5a0bf038e8
dk949 <56653556+dk949@users.noreply.github.com>
2022-11-14 01:06:08 +0000

    added getter for token type

--- source/tokenizer/token.d
+++ source/tokenizer/token.d
@@ -33,16 +33,20 @@ if (isSomeString!S) {
 enum isTokStr(TS) = __traits(isSame, TemplateOf!(TS), TokStr);
 
 struct Token {
-    private TokenType type;
+    private TokenType m_type;
     private Payload payload;
 
+    TokenType type() const {
+        return m_type;
+    }
+
     bool opEquals(TokenType t) const pure nothrow {
-        return t == type;
+        return t == m_type;
     }
 
     bool opEquals(N)(N num) const pure nothrow
     if (isNumeric!N) {
-        if (type == TokenType.NUMBER)
+        if (m_type == TokenType.NUMBER)
             return payload.num == num;
         else
             return false;
@@ -50,7 +54,7 @@ struct Token {
 
     bool opEquals(S)(S str) const pure
     if (isSomeString!S) {
-        if (type == TokenType.ID)
+        if (m_type == TokenType.ID)
             return payload.str == str.text;
         else
             return false;
@@ -58,15 +62,15 @@ struct Token {
 
     bool opEquals(TS)(TS tokS) const pure
     if (isTokStr!TS) {
-        if (type == TokenType.STRING)
+        if (m_type == TokenType.STRING)
             return payload.str == tokS.s.text;
         else
             return false;
     }
 
     bool opEquals(Token other) const pure nothrow {
-        if (type == other.type)
-            with (TokenType) final switch (type) {
+        if (m_type == other.m_type)
+            with (TokenType) final switch (m_type) {
             case ID:
                 return payload.str == other.payload.str;
             case NUMBER:
@@ -81,29 +85,29 @@ struct Token {
     }
 
     this(double num) pure nothrow {
-        type = TokenType.NUMBER;
+        m_type = TokenType.NUMBER;
         payload.num = num;
     }
 
     this(S)(S str) pure
     if (isSomeString!S) {
-        type = TokenType.ID;
+        m_type = TokenType.ID;
         payload.str = str.text;
     }
 
     this(TS)(TS tokS) pure
     if (isTokStr!TS) {
-        type = TokenType.STRING;
+        m_type = TokenType.STRING;
         payload.str = tokS.s.text;
     }
 
     this(TokenType tok) pure nothrow {
-        type = tok;
+        m_type = tok;
         payload = payload.init;
     }
 
     string toString() const pure nothrow {
-        with (TokenType) final switch (type) {
+        with (TokenType) final switch (m_type) {
             case ID:
                 return "Id(" ~ payload.str ~ ")";
             case NUMBER:
@@ -118,13 +122,13 @@ struct Token {
     }
 
     auto to(TokenType tok)() const pure {
-        if (type == tok) {
+        if (m_type == tok) {
             static if (tok == TokenType.NUMBER)
                 return payload.num;
             else static if (tok == TokenType.ID || tok == TokenType.STRING)
                 return payload.str;
         } else
-            throw new TokenException("Cannot convert " ~ type.text ~ " to " ~ tok.text);
+            throw new TokenException("Cannot convert " ~ m_type.text ~ " to " ~ tok.text);
     }
 }
 
@@ -134,8 +138,8 @@ unittest {
         const zero = Token(0);
         const number = Token(879.253489);
 
-        assert(zero.type == NUMBER && zero.payload.num == 0, "number token 0");
-        assert(number.type == NUMBER && number.payload.num == 879.253489, "number token 879.253489");
+        assert(zero.m_type == NUMBER && zero.payload.num == 0, "number token 0");
+        assert(number.m_type == NUMBER && number.payload.num == 879.253489, "number token 879.253489");
     }
 }
 
@@ -149,12 +153,12 @@ unittest {
         const textWId = Token("hello"w);
         const textDId = Token("hello"d);
 
-        assert(emptyId.type == ID && emptyId.payload.str == "", "empty utf8 ID");
-        assert(emptyWId.type == ID && emptyWId.payload.str.wtext == ""w, "empty utf16 ID");
-        assert(emptyDId.type == ID && emptyDId.payload.str.dtext == ""d, "empty utf32 ID");
-        assert(textId.type == ID && textId.payload.str == "hello", `"hello" utf8 ID`);
-        assert(textWId.type == ID && textWId.payload.str.wtext == "hello"w, `"hello" utf16 ID`);
-        assert(textDId.type == ID && textDId.payload.str.dtext == "hello"d, `"hello" utf32 ID`);
+        assert(emptyId.m_type == ID && emptyId.payload.str == "", "empty utf8 ID");
+        assert(emptyWId.m_type == ID && emptyWId.payload.str.wtext == ""w, "empty utf16 ID");
+        assert(emptyDId.m_type == ID && emptyDId.payload.str.dtext == ""d, "empty utf32 ID");
+        assert(textId.m_type == ID && textId.payload.str == "hello", `"hello" utf8 ID`);
+        assert(textWId.m_type == ID && textWId.payload.str.wtext == "hello"w, `"hello" utf16 ID`);
+        assert(textDId.m_type == ID && textDId.payload.str.dtext == "hello"d, `"hello" utf32 ID`);
     }
 }
 
@@ -170,12 +174,12 @@ unittest {
         const textWStr = Token(tokStr("hello"w));
         const textDStr = Token(tokStr("hello"d));
 
-        assert(emptyStr.type == STRING && emptyStr.payload.str == "", "empty utf8 string");
-        assert(emptyWStr.type == STRING && emptyWStr.payload.str.wtext == ""w, "empty utf16 string");
-        assert(emptyDStr.type == STRING && emptyDStr.payload.str.dtext == ""d, "empty utf32 string");
-        assert(textStr.type == STRING && textStr.payload.str == "hello", `"hello" utf8 string`);
-        assert(textWStr.type == STRING && textWStr.payload.str.wtext == "hello"w, `"hello" utf16 string`);
-        assert(textDStr.type == STRING && textDStr.payload.str.dtext == "hello"d, `"hello" utf32 string`);
+        assert(emptyStr.m_type == STRING && emptyStr.payload.str == "", "empty utf8 string");
+        assert(emptyWStr.m_type == STRING && emptyWStr.payload.str.wtext == ""w, "empty utf16 string");
+        assert(emptyDStr.m_type == STRING && emptyDStr.payload.str.dtext == ""d, "empty utf32 string");
+        assert(textStr.m_type == STRING && textStr.payload.str == "hello", `"hello" utf8 string`);
+        assert(textWStr.m_type == STRING && textWStr.payload.str.wtext == "hello"w, `"hello" utf16 string`);
+        assert(textDStr.m_type == STRING && textDStr.payload.str.dtext == "hello"d, `"hello" utf32 string`);
     }
 }
 
@@ -188,7 +192,7 @@ unittest {
         const lbrack = Token(LBRACKET);
         const rbrack = Token(RBRACKET);
 
-        assert(emptyId.type == ID && emptyId.payload.str == null, "empty ID");
+        assert(emptyId.m_type == ID && emptyId.payload.str == null, "empty ID");
         assert(emptyNum == NUMBER && emptyNum.payload.num == 0, "empty number");
         assert(emptyStr == STRING && emptyStr.payload.str == null, "empty string");
         assert(lbrack == LBRACKET && lbrack.payload.null_ == null, "left bracket");
