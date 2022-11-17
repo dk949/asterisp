3d5de1263794d16c69db1797b1ac08d9f47980d1
dk949 <56653556+dk949@users.noreply.github.com>
2022-11-17 22:40:41 +0000

    removed unused function

--- source/tokenizer/utils.d
+++ source/tokenizer/utils.d
@@ -2,36 +2,6 @@ module tokenizer.utils;
 
 import std.traits;
 
-bool isSpaceStr(S)(S str) pure nothrow {
-    return str.all!isWhite.assertNotThrown;
-}
-
-unittest {
-
-    auto trueTests = [
-        "empty": "",
-        "one space": " ",
-        "two spaces": "  ",
-        "newline": "\n",
-        "space and newline": " \n",
-    ];
-
-    auto falseTests = [
-        "one char": "a",
-        "two chars": "ab",
-        "char and space": " b",
-        "char and space 2": "b ",
-        "many spaces and char": "    \n    \t     )        ",
-    ];
-
-    foreach (msg, test; trueTests)
-        assert(test.isSpaceStr, msg);
-
-    foreach (msg, test; falseTests)
-        assert(!test.isSpaceStr, msg);
-
-}
-
 ElementType!R* begin(R)(auto ref R r)
 if (!isSomeString!R && isInputRange!R) {
     return &r.front;
