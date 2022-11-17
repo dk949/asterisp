45ad1d1262fa6910d6c8c38e1990c39b50200ecd
dk949 <56653556+dk949@users.noreply.github.com>
2022-11-17 19:59:21 +0000

    removed source from app

--- source/app.d
+++ source/app.d
@@ -8,20 +8,6 @@ import parser;
 import std.stdio;
 import std.file;
 
-enum tmpSOurce = `
-(*Def globalVar1 42)
-(*Def globalVar2 "hello")
-(*Module helloWorld
-    (*Def modVar "world")
-    (*Def AAAAA ($ var)
-        (*Print var)
-    )
-    (*Def Main
-        (*Print globalVar2 " " modVar " " AAAAA)
-    )
-)
-`;
-
 void main(string[] argv) {
     auto parsedArgs = Args(argv);
 
