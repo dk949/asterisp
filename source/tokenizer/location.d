6f273ef9dc77277f141562aeec455c4cd77b497e
dk949 <56653556+dk949@users.noreply.github.com>
2023-01-02 01:28:26 +0200

    added toString to location

--- source/tokenizer/location.d
+++ source/tokenizer/location.d
@@ -1,12 +1,14 @@
 module tokenizer.location;
 import tokenizer.utils;
+import utils;
 
 import std.algorithm;
+import std.conv;
 
 struct Loc {
-    ulong line;
-    ulong col;
-    string file;
+    ulong line = 0;
+    ulong col = 0;
+    string file = null;
 
     this(const(string) slice, string filename) {
         line = slice.count('\n') + 1;
@@ -25,5 +27,13 @@ struct Loc {
         this.line = line;
         this.col = col;
         this.file = filename;
+    }
+
+    pure
+    string toString() const {
+        const filename = file.coal("UNKNOWN");
+        const lineNo = line ? line.to!string : "UNKOWN";
+        const colNo = col ? col.to!string : "UNKOWN";
+        return filename ~ ":" ~ lineNo ~ ":" ~ colNo;
     }
 }
